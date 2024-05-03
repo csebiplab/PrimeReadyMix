4c56a7b0d2707d91c7282db0416ce7dfe5d7d29b
Oliulla <razasm29@gmail.com>
2024-05-04 01:39:39 +0600

    :tada: New page setup done

---
+++ src/app/(barrie)/barrie-concrete-ready-mix-company/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BarrieConcreteReadyMixPage = () => {
+    return (
+        <div>
+            barrie-concrete-ready-mix-company
+        </div>
+    );
+};
+
+export default BarrieConcreteReadyMixPage;
