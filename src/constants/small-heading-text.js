d442a4a9fec041bdff4fccbe4fcf5cc64cd99ef1
tonmoy <tonmoy3673@gmail.com>
2024-05-07 21:35:00 +0600

    create building standards section

--- src/constants/small-heading-text.js
+++ src/constants/small-heading-text.js
@@ -8,6 +8,7 @@ export const smallHeadingText = {
     building__heading: "Building Success Together",
     power__heading: "Power of Ready Mix Innovation",
     choosing__heading: "Key Factors for Choosing US",
+    standards__heading: "Elevating Barrie's Building Standards",
 
     servicesArea__heading: "Services Area",
     DosDont__heading: "Do’s & Don’t",
