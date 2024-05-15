b92766dbe48c68f428c9754a4707a641277ae9fc
Oliulla <razasm29@gmail.com>
2024-05-16 02:22:21 +0600

    fix all blogs fetching loading problem

--- src/app/(pages)/blogs/BlogPage.js
+++ src/app/(pages)/blogs/BlogPage.js
@@ -1,40 +1,24 @@
-'use client'
-
-
-import React, { Suspense, useEffect, useState } from 'react';
 import { AiOutlineMail } from "react-icons/ai";
 import { FiPhoneCall } from 'react-icons/fi';
 import './blog.css'
 import Blogs from './Blogs';
 import Contact from '../../../components/common/Contact';
-import { usePostDataFetching } from '../../../hooks/usePostDataFetching';
-import LoadingSpinner from '../../../components/common/LoadingSpinner';
+import { Suspense } from "react";
 
-const BlogPage = () => {
-    const [startTime, setStartTime] = useState(0);
-    const [endTime, setEndTime] = useState(0);
-    const { data: categories, isLoading, error } = usePostDataFetching();
+async function getData() {
+    const res = await fetch(process.env.NEXT_PUBLIC_LIVE_API + '/api/blogContent', { cache: 'no-store' })
 
-    useEffect(() => {
-        // Record the start time when the component mounts
-        setStartTime(performance.now());
-    }, []);
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
 
-    useEffect(() => {
-        // Record the end time when data fetching is complete
-        if (!isLoading) {
-            setEndTime(performance.now());
-        }
-    }, [isLoading]);
+    return res.json()
+}
 
-    useEffect(() => {
-        if (startTime && endTime) {
-            const duration = (endTime - startTime) / 1000; // Convert milliseconds to seconds
-            console.log(`>>>>>>>>>>>>>>>>>>>
-            Data fetching took ${duration.toFixed(2)} seconds.
-            <<<<<<<<<<<<<<<<<<<<<<<<<<`);
-        }
-    }, [endTime]);
+const BlogPage = async () => {
+    const data = await getData()
+    const { blogContentData } = data ?? {};
+    console.log("--------------->", blogContentData)
 
     return (
         <>
@@ -52,33 +36,31 @@ const BlogPage = () => {
                 </div>
 
                 <section>
-                    {isLoading ? <LoadingSpinner /> : (
-                        <>
-                            <Suspense>
-                                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 my-16 py-6 bg-white'>
-                                    {categories?.map(item => <Blogs key={item._id} item={item} />)}
-                                </div>
-                            </Suspense>
-                        </>
-                    )}
+                    <>
+                        <Suspense>
+                            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 my-16 py-6 bg-white'>
+                                {blogContentData?.map(item => <Blogs key={item._id} item={item} />)}
+                            </div>
+                        </Suspense>
+                    </>
                 </section>
 
                 <section className='mb-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-16 mt-8'>
                     <div className='background px-12 py-16 flex flex-col justify-start items-start'>
-                        <h3 className='text-white'>Turn Key Renovation Process Like No Other</h3>
-                        <p className='text-white'>Planning, Design, Materials, and Construction Under One Roof</p>
+                        <h3 className='text-black'>Turn Key Renovation Process Like No Other</h3>
+                        <p className='text-black'>Planning, Design, Materials, and Construction Under One Roof</p>
                         <div className='flex gap-4 items-center mt-4'>
-                            <h4 className='text-white'><AiOutlineMail /></h4>
+                            <h4 className='text-black'><AiOutlineMail /></h4>
                             <div>
-                                <p className='text-white'>Email Us</p>
-                                <p className='text-white'>sales@easyrenovation.com</p>
+                                <p className='text-black'>Email Us</p>
+                                <p className='text-black'>sales@easyrenovation.com</p>
                             </div>
                         </div>
                         <div className='flex gap-4 items-center'>
-                            <h4 className='text-white'><FiPhoneCall /></h4>
+                            <h4 className='text-black'><FiPhoneCall /></h4>
                             <div>
-                                <p className='text-white'>Call Us</p>
-                                <p className='text-white'>647-496-0826</p>
+                                <p className='text-black'>Call Us</p>
+                                <p className='text-black'>647-496-0826</p>
                             </div>
                         </div>
                     </div>
