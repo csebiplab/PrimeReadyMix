b9f4d096a10a7fd296d0f4df69b4c703a6f953bf
Oliulla <razasm29@gmail.com>
2024-07-06 18:52:55 +0600

    done all

--- src/app/(pages)/barrie-concrete-ready-mix-company/page.js
+++ src/app/(pages)/barrie-concrete-ready-mix-company/page.js
@@ -1,6 +1,35 @@
 import React from 'react';
 import BarrieConcreteReadyMixPage from './Barrie';
 
+const staticBackupMetadata = {
+    title: "Barrie Concrete",
+    keywords: "Barrie Concrete",
+    description: "barrie concrete, ready mix company",
+}
+
+
+export async function generateMetadata() {
+    try {
+        const apiUrl = process.env.NEXT_PUBLIC_LIVE_API;
+
+        const metaDataResponse = await fetch(`${apiUrl}/api/barrie-concrete`, {
+            cache: "no-store",
+        });
+        const metaData = await metaDataResponse.json();
+
+
+        const { title, description, keywords } = metaData?.data?.[0] ?? {};
+
+        return {
+            title: title,
+            description: description,
+            keywords: keywords
+        }
+    } catch (error) {
+        return staticBackupMetadata;
+    }
+}
+
 const BarriePage = () => {
     return (
         <div>
