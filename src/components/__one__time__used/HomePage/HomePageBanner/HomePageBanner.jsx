b8e9edae29eaf98fbbbc79e08a40dfcd00c38215
Oliulla <mohammad.oliullah25@gmail.com>
2025-01-15 01:08:16 +0600

    Handle banner by one layout for all devices

--- src/components/__one__time__used/HomePage/HomePageBanner/HomePageBanner.jsx
+++ src/components/__one__time__used/HomePage/HomePageBanner/HomePageBanner.jsx
@@ -6,14 +6,16 @@ const HomePageBanner = () => {
   return (
     <>
       <div className="custom-container">
-        {/* large device  */}
-        <div className="hidden md:block md:flex items-center relative">
-          <div className="w-full md:w-[50%] pl-3 md:pl-5 lg:pl-10 xl:pl-20 2xl:pl-28 3xl:pl-40 4xl:pl-52 5xl:pl-[285px]">
-            <div className="py-14 md:py-0 px-4 md:pl-0">
-              <h1 className="text-[34px] md:text-[37px] lg:text-[40px] xl:text-[43px] 2xl:text-[46px] 3xl:text-[49px] 4xl:text-[52px] 5xl:text-[55px] leading-[48px] 5xl:leading-[60px] font-bold text-left text-dark">
+        {/* large device */}
+        <div className="md:flex items-center relative small_device_bg">
+          <div className="w-full md:w-[50%] pl-3 md:pl-5 lg:pl-10 xl:pl-20 2xl:pl-28 3xl:pl-40 4xl:pl-52 5xl:pl-[285px] relative">
+            {/* Overlay using TailwindCSS */}
+            <div className="absolute inset-0 bg-black opacity-50 z-1 block md:hidden"></div>
+            <div className="py-14 md:py-0 px-4 md:pl-0 relative z-10">
+              <h1 className="text-[34px] md:text-[37px] lg:text-[40px] xl:text-[43px] 2xl:text-[46px] 3xl:text-[49px] 4xl:text-[52px] 5xl:text-[55px] leading-[48px] 5xl:leading-[60px] font-bold text-left text-white md:text-dark">
                 Ready Mix Concrete & Concrete Delivery, Toronto Ready Mix
               </h1>
-              <p className="text-[20px] font-semibold leading-6 mt-[5px] text-dark">
+              <p className="text-[20px] font-semibold leading-6 mt-[5px] text-white md:text-dark">
                 When you need reliable and trusted ready mix concrete services
                 in Toronto, there is no better choice than Prime Ready Mix.
               </p>
@@ -37,30 +39,6 @@ const HomePageBanner = () => {
               alt="hero small image"
               className="hero-main-image mb-0"
             />
-          </div>
-        </div>
-        {/* small device  */}
-        <div className="block md:hidden">
-          <div className="small_device_bg">
-            <div className="py-14 md:py-0 px-4 md:pl-0">
-              <h1 className="text-[34px] md:text-[37px] lg:text-[40px] xl:text-[43px] 2xl:text-[46px] 3xl:text-[49px] 4xl:text-[52px] 5xl:text-[55px] leading-[48px] 5xl:leading-[60px] font-bold text-center text-white">
-                Ready Mix Concrete <br/> & Concrete Delivery, <br/> Toronto Ready Mix
-              </h1>
-              <p className="text-[20px] font-semibold leading-6 mt-[5px] text-white text-center">
-                When you need reliable and <br/> trusted ready mix concrete services <br/>
-                in Toronto, there is no better choice <br/> than Prime Ready Mix.
-              </p>
-              <div className="flex justify-center items-center mt-[35px] md:mt-[40px] xl:mt-[45px] 3xl:mt-[50px] 5xl:mt-[55px]">
-                <a href={`tel:+1(647) 449 9512`}>
-                  <button
-                    aria-label="Get an estimate"
-                    className="px-[20px] py-[12px] hero_btn text-white text-sm font-medium leading-[20px] capitalize"
-                  >
-                    <span className="text-white">Get Your Estimate</span>
-                  </button>
-                </a>
-              </div>
-            </div>
           </div>
         </div>
       </div>
